f557958cf76af101f6e7943ae247aa0172e40cd1
stino-x <107426247+stino-x@users.noreply.github.com>
2024-08-16 20:33:38 +0200

    chore: Update MongoDB connection string
    This commit updates the MongoDB connection string in the `connectToMongoDB` function to use the `CONNECTION` environment variable instead of `ELTECONNECTIONSTRING`. This change was made to ensure consistency with the deployment environment.

--- backend/db/connecttoDB.js
+++ backend/db/connecttoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.ELTECONNECTIONSTRING);
+		await mongoose.connect(process.env.CONNECTION);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
